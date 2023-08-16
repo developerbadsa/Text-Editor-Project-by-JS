9f8de95cbdb3397185a9184652a597d14b5dfff0
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-08-17 00:18:03 +0600

    text bold underline and italic maker by js done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,40 @@
-const fontChanger = document.getElementById('fontChanger');
-const alignChanger = document.getElementById('alignChanger');
-const fntAndcltChanger = document.getElementById('fntAndcltChanger');
+
+// font changer
+
+const fontBold = document.getElementById('fontBold');
+const fontItalic = document.getElementById('fontItalic');
+const fontUnderline = document.getElementById('fontUnderline');
+// const inputText = document.getElementById('inputText');
+
+function fontChanger(target){
+
+    const inputText = document.getElementById('inputText');
+
+    switch(target.id){
+        case 'fontBold': inputText.style.fontWeight = 'bold'
+        break;
+        case 'fontItalic': inputText.style.fontStyle = 'italic'
+        break;
+        case 'fontUnderline': inputText.style.textDecoration = 'underline'
+        break;
+
+    }
+    
+    console.log(target.id === 'fontBold')
+
+}
+
+
+
+
+fontBold.addEventListener('click',function(){
+    fontChanger(this)
+});
+fontItalic.addEventListener('click',function(){
+    fontChanger(this)
+});
+fontUnderline.addEventListener('click',function(){
+    fontChanger(this)
+});
+
+
